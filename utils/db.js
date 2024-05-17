75df3b7f88b41b2f370707cc40c32e61094e84d6
Pavel Olyšar <olysarp@gmail.com>
2024-05-17 10:01:17 +0200

    env variables

--- utils/db.js
+++ utils/db.js
@@ -1,8 +1,7 @@
 import { MongoClient, ServerApiVersion } from 'mongodb';
-const uri = "mongodb+srv://andrejpecena:OdZw1d2UppxpYBBv@webtoolkit-free.hre9mqj.mongodb.net/?retryWrites=true&w=majority&appName=WebToolKit-free";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
+const client = new MongoClient(process.env.URI, {
     serverApi: {
         version: ServerApiVersion.v1,
         strict: true,
