e9cacf30b824d22fdcdc037e1a79e3fd345c0a18
Pavel Olyšar <olysarp@gmail.com>
2024-07-22 15:28:00 +0200

    global scss styles and reset css

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,42 +1,68 @@
 <template>
-    <!-- Vercel speed insights -->
-    <SpeedInsights />
-    <!--------------------------->
+    <!-- Vercel speed insights <SpeedInsights />-->
     <div>
-        <nav>
-            <NuxtLink to="/">WebToolKit</NuxtLink>
-            <ul>
-                <li>
-                    <NuxtLink to="/wiki">Wiki</NuxtLink>
-                </li>
-                <li>
-                    <NuxtLink to="/wiki/contribute">Contribute</NuxtLink>
-                </li>
-                <li>
-                    <NuxtLink to="/dashboard">Dashboard</NuxtLink>
-                </li>
-                <li>
+        <header>
+            <nav>
+                <NuxtLink to="/" class="logo">WebToolKit</NuxtLink>
+                <ul>
+                    <li>
+                        <NuxtLink to="/explore">Explore</NuxtLink>
+                    </li>
+                    <li>
+                        <NuxtLink to="/quiz">Quiz</NuxtLink>
+                    </li>
+                    <li>
+                        <NuxtLink to="/wiki">Wiki</NuxtLink>
+                    </li>
+                </ul>
+                <div>
+                    <button><NuxtLink to="/wiki/contribute">Add a Tool</NuxtLink></button>
                     <button @click="openModal">Log In</button>
-                </li>
-            </ul>
-            <Modal :is-open="isModalOpen" @close="closeModal">
-                <LoginForm />
-            </Modal>
-        </nav>
-    </div>
-    <div>
-        <slot />
+                </div>
+                <Modal :is-open="isModalOpen" @close="closeModal">
+                    <LoginForm />
+                </Modal>
+            </nav>
+        </header>
+
+        <main>
+            <slot />
+        </main>
+
+        <footer>
+            <!-- Add your footer content here -->
+            <p>2024 &copy; WebToolKit s.r.o.</p>
+            <p>Made by web-devs for web-devs.</p>
+            <div>
+                <i>ikona</i>
+                <i>ikona</i>
+                <i>ikona</i>
+            </div>
+        </footer>
     </div>
 </template>
 
 <script setup lang="ts">
-// Vercel speed insights
-import { SpeedInsights } from "@vercel/speed-insights/nuxt";
-// ---------------------
+// Vercel speed insights      import { SpeedInsights } from "@vercel/speed-insights";
+
 
 const { isModalOpen, openModal, closeModal } = useModal()
 </script>
 
 <style lang="scss" scoped>
+.layout {
+    display: flex;
+    flex-direction: column;
+    min-height: 100vh;
+}
 
+/*header {}
+
+nav {}
+
+main {
+    flex: 1;
+}
+
+footer {}*/
 </style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,27 +3,12 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   compatibilityDate: "2024-07-09",
 
-  nitro: {
-    experimental: {
-      openAPI: true
-    },
-    plugins: ['~/server/plugins/mongodb.ts'],
-  },
   runtimeConfig: {
     mongodbUri: process.env.MONGODB_URI
   },
 
-  typescript: {
-    strict: true,
-    typeCheck: true,
-    tsConfig: {
-      compilerOptions: {
-        strict: true,
-        types: ['@types/node', '@nuxt/types'],
-        "moduleResolution": "node",
-        "target": "es2022",
-        "module": "esnext",
-      }
-    }
-  }
+  css: [
+    '~/assets/styles/global.scss', 
+    '~/assets/styles/reset.css',
+  ],
 })
