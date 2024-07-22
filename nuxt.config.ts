bbf2433e51548b2c9e8641c2d2467e04e13b4151
Pavel Olyšar <olysarp@gmail.com>
2024-07-22 18:02:23 +0200

    added colors and fonts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,7 +8,22 @@ export default defineNuxtConfig({
   },
 
   css: [
-    '~/assets/styles/global.scss', 
     '~/assets/styles/reset.css',
+    '~/assets/styles/global.scss',
   ],
+
+  app: {
+    head: {
+      link: [
+        {
+          rel: 'stylesheet', // Kanit font
+          href: 'https://fonts.googleapis.com/css2?family=Kanit:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap'
+        },
+        {
+          rel: 'stylesheet', // Ubuntu font
+          href: 'https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap'
+        }
+      ]
+    }
+  }
 })
