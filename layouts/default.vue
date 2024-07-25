7f60e0ce2f6b9b6f9d42f9997b98cebe903b64fa
Pavel Olyšar <olysarp@gmail.com>
2024-07-25 14:30:14 +0200

    default vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,23 +1,25 @@
 <template>
     <!-- Vercel speed insights <SpeedInsights />-->
-    <div>
-        <header>
-            <nav>
-                <NuxtLink to="/" class="logo">WebToolKit</NuxtLink>
-                <ul>
-                    <li>
-                        <NuxtLink to="/explore">Explore</NuxtLink>
+    <div class="layout">
+        <header class="layout__header">
+            <nav class="layout__nav">
+                <NuxtLink class="nav__logo" to="/">WebToolKit</NuxtLink>
+                <ul class="nav__list">
+                    <li class="item nav__item">
+                        <NuxtLink class="link item__link" to="/explore">Explore</NuxtLink>
                     </li>
-                    <li>
-                        <NuxtLink to="/quiz">Quiz</NuxtLink>
-                    </li>
-                    <li>
-                        <NuxtLink to="/wiki">Wiki</NuxtLink>
+                    <li class="item nav__item">
+                        <NuxtLink class="link item__link" to="/quiz">Quiz</NuxtLink>
+                    </li >
+                    <li class="item nav__item">
+                        <NuxtLink class="link item__link" to="/wiki">Wiki</NuxtLink>
                     </li>
                 </ul>
-                <div>
-                    <button><NuxtLink to="/wiki/contribute">Add a Tool</NuxtLink></button>
-                    <button @click="openModal">Log In</button>
+                <div class="nav__btns btns">
+                    <button class="btn btn--primary">
+                        <NuxtLink to="/wiki/contribute">Add a Tool</NuxtLink>
+                    </button>
+                    <button class="btn btn--secondary" @click="openModal">Log In</button>
                 </div>
                 <Modal :is-open="isModalOpen" @close="closeModal">
                     <LoginForm />
@@ -29,13 +31,57 @@
             <slot />
         </main>
 
-        <footer>
-            <p>2024 &copy; WebToolKit s.r.o.</p>
-            <p>Made by web-devs for web-devs.</p>
-            <div>
-                <i>ikona</i>
-                <i>ikona</i>
-                <i>ikona</i>
+        <footer class="layout__footer">
+            <div class="footer__links">
+                <div class="links">
+                    <p class="links__title">Use cases</p>
+                    <ul class="links__list list">
+                        <li class="item footer__item">
+                            <NuxtLink class="link item__link" to="/">UI design</NuxtLink>
+                        </li>
+                        <li class="item footer__item">
+                            <NuxtLink class="link item__link" to="/">UX design</NuxtLink>
+                        </li>
+                        <li class="item footer__item">
+                            <NuxtLink class="link item__link" to="/">Wireframing</NuxtLink>
+                        </li>
+                    </ul>
+                </div>
+                <div class="links">
+                    <p class="links__title">Explore</p>
+                    <ul class="links__list list">
+                        <li class="item footer__item">
+                            <NuxtLink class="link item__link" to="/">Design</NuxtLink>
+                        </li>
+                        <li class="item footer__item">
+                            <NuxtLink class="link item__link" to="/">Prototyping</NuxtLink>
+                        </li>
+                        <li class="item footer__item">
+                            <NuxtLink class="link item__link" to="/">Development features</NuxtLink>
+                        </li>
+                    </ul>
+                </div>
+                <div class="links">
+                    <p class="links__title">User</p>
+                    <ul class="links__list list">
+                        <li class="item footer__item">
+                            <NuxtLink class="link item__link" to="/">Public profile</NuxtLink>
+                        </li>
+                        <li class="item footer__item">
+                            <NuxtLink class="link item__link" to="/">Account settings</NuxtLink>
+                        </li>
+                        <li class="item footer__item">
+                            <NuxtLink class="link item__link" to="/">Colors</NuxtLink>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <p class="footer__text--blue">2024 &copy; WebToolKit s.r.o.</p>
+            <p class="footer__text--gray">Made by web-devs for web-devs.</p>
+            <div class="footer__icons">
+                <i class="footer__icon">ikona</i>
+                <i class="footer__icon">ikona</i>
+                <i class="footer__icon">ikona</i>
             </div>
         </footer>
     </div>
