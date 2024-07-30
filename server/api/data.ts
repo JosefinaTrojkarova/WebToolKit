bb75783665b8a2b9838129b5915c5f5e39648d9f
Pavel Olyšar <olysarp@gmail.com>
2024-07-30 08:00:08 +0200

    atlas search special characters bug fixed

--- server/api/data.ts
+++ server/api/data.ts
@@ -60,6 +60,7 @@ export default defineEventHandler(async (event) => {
                   autocomplete: {
                     query: searchQuery,
                     path: 'name',
+                    tokenOrder: 'sequential',
                   },
                 },
                 {
