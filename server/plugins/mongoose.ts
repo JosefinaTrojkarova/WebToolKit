3629cb5dfcc4b3e25d604065395b20ef865a47fa
Pavel Olyšar <olysarp@gmail.com>
2024-11-26 18:56:19 +0100

    fix better connection to database

--- server/plugins/mongoose.ts
+++ server/plugins/mongoose.ts
@@ -1,17 +1,15 @@
 // Plugin to connect to database
-
 import mongoose from 'mongoose';
 
-export default defineNitroPlugin(async (nitroApp) => {
-  try {
-    await connectToDatabase();
-
-    nitroApp.hooks.hook('request', async () => {
-      if (!mongoose.connection.readyState) {
-        await connectToDatabase();
-      }
-    });
-  } catch (error) {
+export default defineNitroPlugin((nitroApp) => {
+  connectToDatabase().catch((error) => {
     console.error('Failed to connect to MongoDB in plugin:', error);
-  }
+    process.exit(1);
+  });
+
+  nitroApp.hooks.hook('request', async () => {
+    if (!mongoose.connection.readyState) {
+      await connectToDatabase();
+    }
+  });
 });
